54dcc79026e1d73100266671d663a7358aea9f88
K88 <kiddchantw@gmail.com>
2020-06-17 16:04:42 +0800

    feat basic validator

--- app/Http/Controllers/TaskApi.php
+++ app/Http/Controllers/TaskApi.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Tasks;
 use Session;
 use Log;
-
 
 class TaskApi extends Controller
 {
@@ -18,10 +18,10 @@ class TaskApi extends Controller
     public function index()
     {
         //
-       // $taskShow = Tasks::all();
-       // echo  $taskShow;
-       echo  Tasks::all();
-   }
+        // $taskShow = Tasks::all();
+        // echo  $taskShow;
+        echo  Tasks::all();
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -31,46 +31,55 @@ class TaskApi extends Controller
      */
     public function store(Request $request)
     {
-        //todo : 沒成功
-        // return Tasks::create($request->all());
-        // $request->validate([
-        //     'user_id' => 'required' ,
-        //     'content' => 'required' ,
-        //     'creat_at' => 'required'
-        // ]);
 
-        // $rules = array('user_id' => 'required' ,
-        //     'content' => 'required' ,
-        //     'creat_at' => 'required');
-        // $this->validate( $request , $rules);
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'content' => 'required'
+        ]);
 
 
-        // 'user_id'=> $addTaskuserId,
-        // 'content'=> $taskContent,
-        // 'creat_at'=> date("Y-m-d H:i:s",(time()+8*3600))
+
+        if ($validator->fails()) {
+            $messages = $validator->errors()->all();
+            $msg = $messages[0];
+            return response()->json(['success_code' => 401, 'response_code' => 0, 'response_message' => $msg]);
+        }
+
+
+        // $rules = [
+        //     'user_id' => 'required|min:1',
+        //     'content' => 'required|min:1'
+        // ];
+        // $messages = [
+        //     'user_id.required' => '請輸入id',
+        //     'content.required' => '請輸入文章內容',
+        // ];
+
+        // validate($rules, $messages);
+
+
+
+   
 
         $data = array(
             array(
-                'user_id'=> $request->user_id,
-                'content'=> $request->content,
-                'creat_at'=> date("Y-m-d H:i:s",(time()+8*3600))
+                'user_id' => $request->user_id,
+                'content' => $request->content,
+                'creat_at' => date("Y-m-d H:i:s", (time() + 8 * 3600))
             )
         );
         Log::info($data);
 
-        $taskAdd = Tasks::insert($data); 
-    
-        if ($taskAdd == true){
+        $taskAdd = Tasks::insert($data);
+
+        if ($taskAdd == true) {
             return response()->json("add success", 201);
-        }else{
+        } else {
             return response()->json("add 失敗", 201);
 
             //todo : 沒成功。故意用user id = 2(沒此人)
             //postmans是顯示各著Illuminate\Database\QueryException: SQLSTATE[23000]: Integrity constraint violation: 1452 Cannot add or update a child row: a foreign key constraint fails (`dbtest001`.`tasks`, CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)) (SQL: insert into `tasks` (`content`, `creat_at`, `user_id`) values (test5566, 2020-06-16 20:25:56, 2)) in file /usr/local/var/wwwa/testLa/todolistV3/vendor/laravel/framework/src/Illuminate/Database/Connection.php on line 671
-
         }
-
-
     }
 
     /**
@@ -82,7 +91,7 @@ class TaskApi extends Controller
     public function show($id)
     {
         //
-        $taskShow = Tasks::where('user_id','=',$id)->get();
+        $taskShow = Tasks::where('user_id', '=', $id)->get();
         return $taskShow;
     }
 
@@ -95,16 +104,16 @@ class TaskApi extends Controller
      */
     public function update(Request $request, $id)
     {
-        $updateTaskId = $request->taskId ;
+        $updateTaskId = $request->taskId;
         Log::info($updateTaskId);
         $finshStatus = $request->taskDone; //Tasks::find($updateTaskId)->done;
         $updateInt = 0;
         if ($finshStatus == 0) {
             $updateInt = 1;
-        }else{
+        } else {
             $updateInt = 0;
         }
-        Tasks::where('id','=',$updateTaskId)->update(['done'=>$updateInt]);
+        Tasks::where('id', '=', $updateTaskId)->update(['done' => $updateInt]);
 
         return response()->json('update success');
     }
@@ -118,9 +127,7 @@ class TaskApi extends Controller
     public function destroy($id)
     {
         //
-        $deteleTest = Tasks::where('id','=',$id)->delete();
+        $deleteTest = Tasks::where('id', '=', $id)->delete();
         return response()->json('delete success');
-
-
     }
 }

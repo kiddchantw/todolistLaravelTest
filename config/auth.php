5680bbb08c5ee8bcfb795079a78580371f26e7c7
K88 <kiddchantw@gmail.com>
2020-07-29 02:02:13 +0800

    feat auth.php/provider

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Users::class,
+            // 'model' => App\Users::class,
+            'model' => 'App\\Users',
         ],
 
         // 'users' => [

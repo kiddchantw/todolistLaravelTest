e74cc76609d5bc594d409c9dc8bea5d06b4560e1
K88 <kiddchantw@gmail.com>
2020-08-24 14:51:04 +0800

    fix 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2
+                    Laravel 22
                 </div> 
 
                 <div class="links">

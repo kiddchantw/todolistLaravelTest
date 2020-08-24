e5a126142a427e1faedc8f24b4f8c7825cca31da
K88 <kiddchantw@gmail.com>
2020-08-24 14:40:57 +0800

    fix m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,8 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
+                    Laravel 2
+                </div> 
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
